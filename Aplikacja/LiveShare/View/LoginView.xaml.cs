e2b78f7cc31bbb8ed0c8d13c5a05f638a73d5b64
lshadown <mgruzewski@wi.zut.edu.pl>
2016-03-12 10:26:18 +0100

    Stworzenie struktur aplikacji/

--- Aplikacja/LiveShare/View/LoginView.xaml.cs
+++ Aplikacja/LiveShare/View/LoginView.xaml.cs
@@ -12,20 +12,21 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using LiveShare.ViewModel;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace LiveShare
+namespace LiveShare.View
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class LoginView : Page
     {
-        public MainPage()
+        public LoginView()
         {
             this.InitializeComponent();
-            //test
+            DataContext = new LoginViewModel();
         }
     }
 }
